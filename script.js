8ff5d6175ba918d3e4c0ce4b271cb472ec80e2c6
Soul2606 <sondulv@gmail.com>
2025-09-18 12:51:17 +0200

    message

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ class GridItem {
 	#element
 	constructor(element) {
 		if (!(element instanceof HTMLElement)) throw new Error("invalid element");
+		if (this.#parseGridArea(element) === null) throw new Error("element has an invalid grid area");
 		this.#element = element
 	}
 
@@ -15,11 +16,9 @@ class GridItem {
 	}
 
 	isOverlapping(...otherItem) {
-		console.log(otherItem)
-		if (!(otherItem instanceof GridItem || Array.isArray(otherItem))) throw new Error("Argument must be a GridItem or an Array");
-		if (Array.isArray(otherItem) && otherItem.some(item => !(item instanceof GridItem))) throw new Error("Array contains non GridItem");
+		if (otherItem.some(item => !(item instanceof GridItem))) throw new Error("Array contains non GridItem");
 
-		const array = Array.isArray(otherItem)? otherItem: [otherItem];
+		const array = otherItem
 		const a = this.getGridArea();
 
 		return array.some(item=>{
@@ -33,16 +32,74 @@ class GridItem {
 	}
 
 	#parseGridArea(element) {
-		const area = getComputedStyle(element).getPropertyValue('grid-area').trim();
+		const area = getComputedStyle(element).getPropertyValue('grid-area').trim().split('/');
+		if (area.length !== 4) return null
+		if (area.includes('auto')) return null
 		// Example area = "1 / 2 / 4 / 5"
 		const [rowStart, colStart, rowEnd, colEnd] = area
-			.split('/')
 			.map(s => parseInt(s, 10) || 0);
 		return { rowStart, rowEnd, colStart, colEnd };
+	}
+
+	getSurroundingItems(otherItems){
+		if (!Array.isArray(otherItems)) throw new Error("otherItems is not an array");
+		if (otherItems.some(item => !(item instanceof GridItem))) throw new Error("Array contains non GridItem");
+		const { rowStart, rowEnd, colStart, colEnd } = parseGridArea(this.#element)
+
+		return otherItems.filter(item=>{
+			const area = item.getGridArea()
+
+			// Above
+			if (isOverlapping({ rowStart: rowStart - 1, rowEnd: rowStart, colStart, colEnd }, area)) return true;
+
+			// Below
+			if (isOverlapping({ rowStart: rowEnd, rowEnd: rowEnd + 1, colStart, colEnd }, area)) return true;
+
+			// Left
+			if (isOverlapping({ rowStart, rowEnd, colStart: colStart - 1, colEnd: colStart }, area)) return true;
+
+			// Right
+			if (isOverlapping({ rowStart, rowEnd, colStart: colEnd, colEnd: colEnd + 1 }, area)) return true;
+
+			return false;
+		})
 	}
 }
 
 
+
+
+function parseGridArea(element) {
+	const area = getComputedStyle(element).getPropertyValue('grid-area').trim().split('/');
+	if (area.length !== 4) return null
+	if (area.includes('auto')) return null
+	// Example area = "1 / 2 / 4 / 5"
+	const [rowStart, colStart, rowEnd, colEnd] = area
+		.map(s => parseInt(s, 10) || 0);
+	return { rowStart, rowEnd, colStart, colEnd };
+}
+
+
+
+
+function isOverlapping(area1, area2) {
+	const a = area1
+	const b = area2
+
+	if (!('rowStart' in a && 'rowEnd' in a && 'colStart' in a && 'colEnd' in a)) throw new Error("Error");
+	if (!('rowStart' in b && 'rowEnd' in b && 'colStart' in b && 'colEnd' in b)) throw new Error("Error");
+
+	const colsOverlap = a.colStart < b.colEnd && a.colEnd > b.colStart;
+	const rowsOverlap = a.rowStart < b.rowEnd && a.rowEnd > b.rowStart;
+
+	return colsOverlap && rowsOverlap;
+		
+}
+
+
+
+
 const gridCells = Array.from(document.getElementsByClassName('grid-cell')).map(item => new GridItem(item))
-gridCells[0].isOverlapping([gridCells[1], gridCells[2], gridCells[3]])
+console.log(gridCells)
+gridCells[0].getSurroundingItems([gridCells[1], gridCells[2], gridCells[3]])
 
