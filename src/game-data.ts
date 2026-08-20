88811b34cbdbe0632cb366bfac97940b5d915b4d
Sondre Ulvik <sondulv@gmail.com>
2026-08-20 22:13:24 +0200

    - reworked game data validation

--- src/game-data.ts
+++ src/game-data.ts
@@ -1,6 +1,7 @@
 // =============== NO IMPORT (except types) ================
 import type { ItemSer } from "./crafting-system/types"
 import type { JSONValue } from "./common/types"
+import { validate, type Config } from "./lib/data/json/validator.js";
 
 type Data = Readonly<{
 	items:readonly ItemDef[],
@@ -22,6 +23,24 @@ type ItemSchema = Record<
 	}
 >
 
+const itemSchema:Config = {
+	type:"record",
+	match:{
+		type:"obj",
+		match:{
+			name: {type:"str"},
+			formula: {type:"str", option:true},
+			description: {type:"str", option:true},
+			tags: {
+				type:"arr",
+				match:{type:"str"}
+			},
+			img: {type:"str"},
+			energy: {type:"str"},
+		}
+	}
+}
+
 
 type RecipeSchemaInput = {
 	id:string
@@ -40,6 +59,41 @@ type RecipeSchema = {
 	requiredProcess:string
 	requiredTier?:number
 	processTimeSeconds?:number
+}[]
+
+const recipeSchema:Config = {
+	type:"arr",
+	match:{
+		type:"obj",
+		match:{
+			id:{type:"str"},
+			inputs:{
+				type:"arr",
+				match:{
+					type:"obj",
+					match:{
+						id:{type:"str"},
+						amount:{type:"num"},
+						meta:{type:"any", option:true}
+					}
+				}
+			},
+			outputs:{
+				type:"arr",
+				match:{
+					type:"obj",
+					match:{
+						id:{type:"str"},
+						amount:{type:"num"},
+						meta:{type:"any", option:true},
+					}
+				}
+			},
+			requiredProcess:{type:"str"},
+			requiredTier:{type:"num", option:true},
+			processTimeSeconds:{type:"num", option:true},
+		}
+	}
 }
 
 
@@ -64,6 +118,58 @@ type MachineSchema = Record<string, {
 	}
 }>
 
+const machineSchema:Config = {
+	type:"record",
+	match:{
+		type:"obj",
+		match:{
+			id:{type:"str"},
+			name:{type:"str"},
+			tier:{type:"num"},
+			capabilities:{
+				type:"arr",
+				match:{type:"str"}
+			},
+			cost:{
+				type:"arr",
+				match:{
+					type:"obj",
+					match:{
+						id:{type:"str"},
+						amount:{type:"num"},
+						meta:{type:"any", option:true}
+					}
+				}
+			},
+			img:{type:"str", option:true},
+			fuelNeeds:{
+				type:"obj",
+				option:true,
+				match:{
+					tags: {type:"arr", match:{type:"str"}},
+					energy: {type:"str"}
+				}
+			},
+			energyNeeds:{
+				type:"obj",
+				option:true,
+				match:{
+					voltageTier: {type:"num"},
+					energy: {type:"str"}
+				}
+			},
+			workerNeeds:{
+				type:"obj",
+				option:true,
+				match:{
+					minimum:{type:"num"},
+					maximum:{type:"num"}
+				}
+			}
+		}
+	}
+}
+
 
 type ExtractorSchema = Record<string, {
 	name:string
@@ -75,245 +181,105 @@ type ExtractorSchema = Record<string, {
 	}[]
 }>
 
-
-
-
-function isPlainObject(value: unknown): value is Record<string, unknown> {
-	if (typeof value !== "object" || value === null) return false
-	if (Array.isArray(value)) return false
-
-	const proto = Object.getPrototypeOf(value)
-	return proto === Object.prototype || proto === null
+const extractorSchema:Config = {
+	type:"record",
+	match:{
+		type:"obj",
+		match:{
+			name:{type:"num"},
+			manualPower:{type:"num", option:true},
+			requiredPower:{type:"num"},
+			yields:{
+				type:"arr",
+				match:{
+					type:"obj",
+					match:{
+						itemId:{type:"str"},
+						weight:{type:"num"}
+					}
+				}
+			}
+		}
+	}
 }
 
 
-function isArray(v: unknown): v is unknown[] {
-	return Array.isArray(v)
+
+
+async function fetchJSON<T = any>(url: string): Promise<T> {
+	return fetch(url).then(response => {
+		if (!response.ok) {
+			throw new Error("Network response was not ok" + response.statusText)
+
+		}
+		return response.json()
+	})
 }
 
 
-
-async function fetchData(){
-	
-	async function fetchJSON(url: string) {
-		return fetch(url).then(response => {
-			if (!response.ok) {
-				throw new Error("Network response was not ok" + response.statusText)
-
+async function fetchData():Promise<Data> {
+	const items = await fetchJSON<ItemSchema>('game-data/items.json')
+	const machines = await fetchJSON<MachineSchema>('game-data/machines.json')
+	const recipes = await fetchJSON<RecipeSchema>('game-data/recipes.json')
+	const extraction = await fetchJSON<ExtractorSchema>('game-data/extraction.json')
+	validate(items, itemSchema)
+	validate(machines as any, machineSchema)
+	validate(recipes, recipeSchema)
+	validate(extraction, extractorSchema)
+	return { 
+		items: Object.entries(items as ItemSchema).map(([key, value]) =>{
+			const item = {
+				...value,
+				id:key
 			}
-			return response.json()
-		})
-	}
-
-	function compile(items: unknown, machines: unknown, recipes: unknown, extraction: unknown) {
-
-		if (!isPlainObject(items)) throw new Error("items is not an object")
-		if (!isPlainObject(machines)) throw new Error("machines is not an object")
-		if (!isArray(recipes)) throw new Error("recipes is not an array")
-		if (!isPlainObject(extraction)) throw new Error("extractors is not an object")
-
-
-		const limitKeysTo = (obj: any, keys: string[]) => {
-			if (Object.keys(obj).some(key => !keys.includes(key))) throw new Error(`${obj.id} has invalid keys, object can only have these keys:${keys}`)
-		}
-
-		const includeKeys = (obj: any, keys: string[]) => {
-			if (keys.some(key => !Object.keys(obj).includes(key))) throw new Error(`${obj.id} has invalid keys, object must include these keys:${keys}`)
-		}
-
-		Object.values(items).forEach(item => {
-			includeKeys(item, ['name'])
-		})
-
-		Object.values(machines).forEach(item => {
-			includeKeys(item, ['name', 'capabilities', 'tier', "cost"])
-		})
-		Object.values(machines).forEach(item => {
-			limitKeysTo(item, ['name', 'capabilities', 'tier', "cost", "img", 'energyNeeds', 'fuelNeeds', "workerNeeds"])
-		})
-
-		recipes.forEach(item => {
-			limitKeysTo(item, ['id', 'inputs', 'outputs', 'requiredProcess', 'requiredTier', 'processTimeSeconds'])
-		})
-
-		type Types = "string" | "number" | "boolean" 
-		/** Check primitive type */
-		const pt = (obj: any, type: Types|Types[], optional?: true) => {
-			if (optional && obj === undefined) return
-			const TYPE = [type].flat()
-			const valid = TYPE.some(type => 
-				typeof obj === type
-			)
-			if (!valid) throw new Error(`${obj} is not of type ${JSON.stringify(TYPE)}`)
-		}
-
-		const itemIds = new Set<string>()
-
-		for (const key in items) {
-			const item = items[key]
-			itemIds.add(key)
-			if (!isPlainObject(item)) throw new Error("Item must be a plain object");
-			if(isArray(item.tags)){
-				item.tags.forEach((tag: any) => pt(tag, 'string'))
+			return {
+				id: item.id,
+				name: item.name,
+				formula: item.formula ?? "",
+				description: item.description ?? "",
+				tags: item.tags ?? [],
+				img: item.img ?? "",
+				energy:item.energy 
 			}
-			pt(item.name, 'string')
-			pt(item.img, "string", true)
-			pt(item.formula, "string", true)
-			pt(item.description, "string", true)
-		}
-
-		for (const key in machines) {
-			const machine = machines[key]
-			if (!isPlainObject(machine)) throw new Error("Machine is not an object");
-			pt(machine.name, 'string')
-			pt(machine.tier, 'number')
-			if (isArray(machine.cost)) {				
-				for (const cost of machine.cost) {
-					if (!isPlainObject(cost)) throw new Error("Machine cost is not an array of objects");
-					pt(cost.id, "string")
-					if (!itemIds.has(cost.id as string)) throw new Error("Unknown item id: " + cost.id);
-					pt(cost.amount, "number")
-				}
+		}),
+		machines: Object.entries(machines as MachineSchema).map(([key, value]) => {
+			const machine = {
+				...value,
+				id:key
 			}
-			if (isArray(machine.capabilities)) {
-				machine.capabilities.forEach(item => 
-					pt(item, 'string')
-				)
-			}
-			pt(machine.img, "string", true)
-			if (machine.fuelNeeds) {
-				if (!isPlainObject(machine.fuelNeeds)) throw new Error("fuel needs is not an object");
-				if (!isArray(machine.fuelNeeds.tags)) throw new Error("fuel needs tag is not an array");
-				machine.fuelNeeds.tags.forEach(v => pt(v, 'string'))
-				pt(machine.fuelNeeds.energy, 'string')
-			}
-			if (machine.energyNeeds) {
-				if (!isPlainObject(machine.energyNeeds)) throw new Error("energy needs is not an object");
-				pt(machine.energyNeeds.voltageTier, 'number')
-				pt(machine.energyNeeds.energy, 'string')
+			return {
+				id:machine.id,
+				name:machine.name,
+				tier:machine.tier,
+				capabilities:machine.capabilities,
+				img:machine.img??"",
+				cost:machine.cost.map(item => ({
+					id:item.id,
+					amount:item.amount,
+					metadata: item.meta??null
+				})),
+				fuelNeeds:  machine.fuelNeeds,
+				energyNeeds:machine.energyNeeds,
+				workerNeeds:machine.workerNeeds,
 			}
 		}
-
-		for (const recipe of recipes) {
-			if (!isPlainObject(recipe)) throw new Error("recipe is not a plain object");
-			pt(recipe.id, 'string', true)
-			pt(recipe.requiredProcess, 'string')
-			pt(recipe.requiredTier, 'number', true)
-			pt(recipe.processTimeSeconds, 'number')
-			if (!isArray(recipe.inputs)) throw new Error("inputs is not an array");
-			for(const input of recipe.inputs){
-				if (!isPlainObject(input)) throw new Error("input is not an object");
-				pt(input.amount, 'number')
-				if ("id" in input) {
-					pt(input.id, 'string')
-					if (!itemIds.has(input.id as string)) throw new Error("Unknown item id: " + input.id);
-				} else {
-					pt(input.tag, 'string')
-				}
-			}
-			if (!isArray(recipe.outputs)) throw new Error("outputs is not an array");
-			for (const output of recipe.outputs){
-				if (!isPlainObject(output)) throw new Error("output is not an object");
-				pt(output.id, 'string')
-				if (!itemIds.has(output.id as string)) throw new Error("Unknown item id: " + output.id);
-				pt(output.amount, 'number')
-			}
-		}
-
-
-		for (const key in extraction) {
-			if (!Object.hasOwn(extraction, key)) continue;
-			const ext = extraction[key];
-			if (!isPlainObject(ext)) throw new Error("extractor is not an object");
-			pt(ext.name, "string")
-			pt(ext.manualPower, "number", true)
-			pt(ext.requiredPower, "number")
-			if (!isArray(ext.yields)) throw new Error("yields is not an array");
-			for (const y of ext.yields) {
-				if (!isPlainObject(y)) throw new Error("yield is not an object");
-				pt(y.itemId, "string")
-				pt(y.weight, "number", true)
-			}
-		}
-
-
-		const hasDuplicateIds = (array: string[]) => {
-			const previousIds = new Set<string>()
-			const duplicates = new Set<string>()
-			for (const str of array) {
-				if (previousIds.has(str)) duplicates.add(str)
-				previousIds.add(str)
-			}
-			return duplicates.size === 0 ? false : duplicates
-		}
-		{
-			const result = hasDuplicateIds(Object.keys(items).concat(Object.keys(machines)))
-			if (result) throw new Error(`Machines and Items has duplicate IDs, ${result}`)
-		}
-		{
-			const result = hasDuplicateIds(recipes.map((r:any, i) => r.id??"v-"+i))
-			if (result) throw new Error(`Recipes has duplicate IDs, ${result}`)
-		}
-		return { 
-			items: Object.entries(items as ItemSchema).map(([key, value]) =>{
-				const item = {
-					...value,
-					id:key
-				}
-				return {
-					id: item.id,
-					name: item.name,
-					formula: item.formula ?? "",
-					description: item.description ?? "",
-					tags: item.tags ?? [],
-					img: item.img ?? "",
-					energy:item.energy 
-				}
-			}),
-			machines: Object.entries(machines as MachineSchema).map(([key, value]) => {
-				const machine = {
-					...value,
-					id:key
-				}
-				return {
-					id:machine.id,
-					name:machine.name,
-					tier:machine.tier,
-					capabilities:machine.capabilities,
-					img:machine.img??"",
-					cost:machine.cost.map(item => ({
-						id:item.id,
-						amount:item.amount,
-						metadata: item.meta??null
-					})),
-					fuelNeeds:  machine.fuelNeeds,
-					energyNeeds:machine.energyNeeds,
-					workerNeeds:machine.workerNeeds,
-				}
-			}
-			),
-			recipes: (recipes as RecipeSchema[]).map((r,i) => ({
-				id:r.id??"v-"+i,
-				inputs:r.inputs.map(i=>("id" in i ? {id:i.id, amount:i.amount, meta:i.meta??null} : {tag:i.tag, amount:i.amount, meta:i.meta??null})),
-				outputs:r.outputs.map(i=>({id:i.id, amount:i.amount, metadata:i.meta??null})),
-				requiredProcess: r.requiredProcess,
-				requiredTier: r.requiredTier??0,
-				processTimeSeconds: r.processTimeSeconds??0
-			} satisfies RecipeDef)),
-			extractors:Object.entries(extraction as ExtractorSchema).map(([key,value]) => ({
-				id:key,
-				name:value.name,
-				manualPower:value.manualPower??0,
-				requiredPower:value.requiredPower,
-				yields:value.yields.map(y => ({itemId:y.itemId, weight:y.weight??1}))
-			}))
-		} satisfies Data
-	}
-	const items = await fetchJSON('game-data/items.json')
-	const machines = await fetchJSON('game-data/machines.json')
-	const recipes = await fetchJSON('game-data/recipes.json')
-	const extraction = await fetchJSON('game-data/extraction.json')
-	return compile(items, machines, recipes, extraction)
+		),
+		recipes: (recipes as RecipeSchema).map((r,i) => ({
+			id:r.id??"v-"+i,
+			inputs:r.inputs.map(i=>("id" in i ? {id:i.id, amount:i.amount, meta:i.meta??null} : {tag:i.tag, amount:i.amount, meta:i.meta??null})),
+			outputs:r.outputs.map(i=>({id:i.id, amount:i.amount, metadata:i.meta??null})),
+			requiredProcess: r.requiredProcess,
+			requiredTier: r.requiredTier??0,
+			processTimeSeconds: r.processTimeSeconds??0
+		} satisfies RecipeDef)),
+		extractors:Object.entries(extraction as ExtractorSchema).map(([key,value]) => ({
+			id:key,
+			name:value.name,
+			manualPower:value.manualPower??0,
+			requiredPower:value.requiredPower,
+			yields:value.yields.map(y => ({itemId:y.itemId, weight:y.weight??1}))
+		}))
+	} satisfies Data
 }
 
 
---
+++ src/lib/data/json/validator.ts
@@ -0,0 +1,119 @@
+import type { JSONValue } from "../../types.ts"
+
+type Primitives = "str"|"num"|"bool"|"null"
+
+export type Config = {
+	option?:boolean
+} & (
+	{
+		type:Primitives|"any"
+	}|{
+		type:"record"
+		match:Config
+	}|{
+		type:"obj",
+		match:Record<string, Config>
+	}|{
+		type:"arr"
+		match:Config
+	}|{
+		type:"tuple"
+		match:Config[]
+	}
+)
+
+function isObj(val:JSONValue): val is {[key:string]: JSONValue} {
+	return (typeof val === "object" && val !== null && !Array.isArray(val))
+}
+
+function isArr(val:JSONValue): val is JSONValue[] {
+	return Array.isArray(val)
+}
+
+function checkPrimTypes(str:string, val:JSONValue) {
+	switch (str) {
+		case "str":
+			return typeof val === "string"
+		case "num":
+			return typeof val === "number"
+		case "bool":
+			return typeof val === "boolean"
+		case "null":
+			return val === null
+		default:
+			return true
+	}
+}
+
+type Error = {
+	err:string
+	at:string
+}
+
+function recurse(json:JSONValue, config:Config, path:string[]):Error[] {
+	if (config.type === "any") return []
+	const errors:Error[] = []
+	const add = (str:string) => errors.push({err:str, at:path.join(".")})
+	const conf = config
+
+	if (!checkPrimTypes(conf.type,json)) add("Wrong type")
+
+	switch (conf.type) {
+		case "obj":
+			if (!isObj(json)) {
+				add("wrong type")
+				break
+			} 
+			for (const [key, val] of Object.entries(conf.match)) {
+				const item = json[key]
+				if (item === undefined) {
+					if (!val.option) add(`key "${key}" is missing`)
+				} else {
+					errors.push(...recurse(item, val, [...path, key]))
+				}
+			}
+			break
+		case "record":
+			if (!isObj(json)) {
+				add("wrong type")
+				break
+			} 
+			for (const [key, val] of Object.entries(json)) {
+				if (conf.match.option) continue
+				errors.push(...recurse(val, conf.match, [...path, key]))
+			}
+			break
+		case "arr":
+			if (!isArr(json)) {
+				add("wrong type")
+				break
+			}
+			for (const [idx,val] of json.entries()) {
+				if (conf.match.option) continue
+				errors.push(...recurse(val, conf.match, [...path, idx.toString()]))
+			}
+			break
+		case "tuple":
+			if (!isArr(json)) {
+				add("wrong type")
+				break
+			}
+			if (conf.match.length < json.length) add("Too many items")
+			for (const [idx, val] of conf.match.entries()) {
+				const item = json.at(idx)
+				if (item === undefined) {
+					if (!val.option) add(`item at index "${idx}" is missing`)
+				} else {
+					errors.push(...recurse(item, val, [...path, idx.toString()]))
+				}
+			}
+			break
+	}
+	return errors
+}
+
+
+
+export function validate(json:JSONValue, config:Config) {
+	return recurse(json, config, ["$"])
+}
